7438f08a64d1e1848b31c727ce05d428219026e7
Mohamad Salimi <mohamad.salimi@hotmail.com>
2022-11-02 14:52:57 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+let element = document.querySelector('.scroller')
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
+window.addEventListener('scroll', () => {
+    let scrollTop = document.documentElement.scrollTop;
+    element.style.width = `${(scrollTop / height) * 100}%`;
+});
